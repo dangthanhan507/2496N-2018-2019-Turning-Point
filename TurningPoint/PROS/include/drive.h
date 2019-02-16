3512c4206719b100e9a5b057ba007a9706cc1797
Andrew Valdivia <andrewv115z@gmail.com>
2019-02-15 17:36:31 -0800

    Update drive.h

--- TurningPoint/PROS/include/drive.h
+++ TurningPoint/PROS/include/drive.h
@@ -3,8 +3,11 @@
 
 #ifndef DRIVE_H
 #define DRIVE_H
+
 bool catapult_auto = true;
 bool catapult_load = true;
+
+//********************* DRIVE CATAPULT *****************************
 void drive_catapult()
 {
   if(controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R2)) catapult_auto = !catapult_auto;
@@ -29,17 +32,42 @@ void drive_catapult()
     }
   }
 }
+
+//*********************** DRIVE BALL INTAKE ************************
 void drive_ball_intake()
 {
- //get andrew's code
+// Change motor ports to be with the current robot motor ports 
+// change the rpm for the correct gear set 
+	if (master.get_digital_new_press (E_CONTROLLER_DIGITAL_L1) )
+  {
+		intake_motor.move_velocity(100);// configure the RPM 
+	}
+	else if (master.get_digital_new_press (E_CONTROLLER_DIGITAL_L2) )
+  {
+		intake_motor.move_velocity(-100);// configure the rpm
+	}
+	else 
+  {
+	  intake_motor.move_velocty(0); // stops the motor 
+	}
 }
-void drive_stick()
+
+//***************************** DRIVE STICK *************************
+void drive_stick()// drive for stick 
 {
-  //get andrew's code
+  if (master.get_digital_new_press (E_CONTROLLER_DIGITAL_Y)) 
+   {
+	  Stick.move_velocity(50); 
+	}
+	else if (master.get_digital_new_press (E_CONTROLLER_DIGITAL_X))
+  {
+		Stick.move_velocity(-50);
+	}
 }
+
+///**************************** DRIVE TANK **************************
 void drive_tank()
 {
   //get andrew's code
 }
-
 #endif
