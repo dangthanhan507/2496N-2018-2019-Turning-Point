0f7de662ac8f3112faae9874a29942e98c0ef776
Andrew Valdivia <andrewv115z@gmail.com>
2019-02-15 19:19:12 -0800

    Update drive.h

--- TurningPoint/PROS/include/drive.h
+++ TurningPoint/PROS/include/drive.h
@@ -62,8 +62,47 @@ void drive_stick()// drive for stick {
 }
 
 ///**************************** DRIVE TANK **************************
-void drive_tank()
-{
-  //get andrew's code
+void drive_ball_intake () {
+//decleration
+// Change motor ports to be with the current robot ports
+// chagne the rpm for the correct gear set
+if (master.get_digital_new_press (E_CONTROLLER_DIGITAL_L1) ){
+	intake_motor.move_velocity(100);// configure the RPM
+	}
+else if (master.get_digital_new_press (E_CONTROLLER_DIGITAL_L2) ){
+	intake_motor.move_velocity(-100);// configure the rpm
+	}
+else{
+	intake_motor.move_velocty(0); // stops the motor
+	}
+}
+//*********************** DRIVE TANKS FUNCTION ********************************
+void drive_tank(){
+	reverse_drive();
+	if(drive_reverse) { // checks if toggled
+		 // DECLARE variable in main()
+		 left_axis = -E_CONTROLLER_ANALOG_LEFT_Y;
+		 right_axis = -E_CONTROLLER_ANALOG_RIGHT_Y;
+		 side_axis = -E_CONTROLLER_ANALOG_LEFT_X;
+		}
+	else {//if it is toggled
+		 left_axis = E_CONTROLLER_ANALOG_LEFT_Y;
+		 right_axis = E_CONTROLLER_ANALOG_RIGHT_Y;
+		 side_axis = E_CONTROLLER_ANALOG_LEFT_X;
+  }
+	if(abs(E_CONTROLLER_ANALOG_LEFT_X) > 90 && abs(E_CONTROLLER_ANALOG_LEFT_X) > 80) {
+		//more unknown variables needed to be declared
+		drive_r = -side_axis/3;
+		drive_l = side_axis/3;
+	}
+	else {
+		drive_l = left_axis;
+		drive_r = right_axis;
+	}
+	//MOTORS that need to be declared
+	BackLeft.move(drive_l);
+	FrontLeft.move(drive_l);
+	FrontRight.move(drive_r);
+	BackRight.move(drive_r);
 }
 #endif
